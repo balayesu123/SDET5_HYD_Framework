fb50a019b44f74665b4e31acf2ae7d9e917745a1
sivaram <sivaram@DESKTOP-J1BNB1Q>
2022-04-14 12:07:01 +0530

    new branch Name

---
+++ SDET_HYD_5/src/test/java/pom/github/Brantch/Github3.java
@@ -0,0 +1,11 @@
+package pom.github.Brantch;
+
+import org.testng.annotations.Test;
+
+public class Github3 {
+	@Test
+	  public void gitHub3()
+	  {
+		  String n3="bala";
+	  }
+}
---
+++ SDET_HYD_5/src/test/java/pom/github/Brantch/Github4.java
@@ -0,0 +1,11 @@
+package pom.github.Brantch;
+
+import org.testng.annotations.Test;
+
+public class Github4 {
+	@Test
+	  public void gitHub4()
+	  {
+		  String n4="kumar";
+	  }
+}
